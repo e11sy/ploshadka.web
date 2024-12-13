6f20fabc81d5d5164ecf22be94facba03e30941f
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-13 05:28:04 +0300

    index and app.vue updates

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
   <Header />
-  <RouterView />
+  <RouterView :class="$style['app']"/>
 </template>
 
 <script setup lang="ts">
@@ -10,6 +10,8 @@ import { Header } from '@/presentation/components';
 
 <style module>
 @import './style/typography.pcss';
-
-
+.app {
+  background-color: #F5F5F5;
+  height: 100%;
+}
 </style>
