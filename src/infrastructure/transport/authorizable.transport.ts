cf1408951e8f5e772b8cdc1ecabca7e60fb67ae8
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-13 05:28:45 +0300

    remove redundant logs

--- src/infrastructure/transport/authorizable.transport.ts
+++ src/infrastructure/transport/authorizable.transport.ts
@@ -53,7 +53,6 @@ export default class AuthorizableTransport extends Transport {
    * @param accessToken - JWT to send with the authorization header
    */
   public authorize(accessToken: string): void {
-    console.log('authorize', accessToken);
     this.headers.set('Authorization', `Bearer ${accessToken}`);
 
     this.authState = 'authorized';
