bcc04fb635bee90ff342927541385d4f761269f4
e11sy <130844513+e11sy@users.noreply.github.com>
2024-12-13 05:31:28 +0300

    context menu component

---
+++ src/presentation/components/ContextMenu/ContextMenuItem.vue
@@ -0,0 +1,82 @@
+<template>
+  <div :class="$style['item']">
+    <div
+      v-if="item.type === 'default' || !item.type"
+      :class="[$style['item__default']]"
+    >
+      <div
+        :class="$style['item__body']"
+        @click="item.onActivate"
+      >
+        <Icon
+          v-if="item.icon !== undefined"
+          :name="item.icon"
+        />
+        <div>
+          {{ item.title }}
+        </div>
+      </div>
+    </div>
+    <div
+      v-if="item.type === 'separator'"
+      :class="$style['item__separator']"
+    >
+      <div :class="$style['item__line']" />
+    </div>
+    <div
+      v-if="item.type === 'message'"
+      :class="[$style['item__default'],
+               $style['item__default--no-hover']]"
+    >
+      {{ item.message }}
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import Icon from '../Icon';
+import type { ContextMenuItem } from './ContextMenu.types';
+
+defineProps<{
+  /**
+   * Context menu item, can be default or separator
+   */
+  item: ContextMenuItem;
+}>();
+</script>
+
+<style lang="postcss" module>
+.item {
+  &__separator {
+    padding: 2px 4px;
+  }
+
+  &__line {
+    align-self: stretch;
+    background: #FFFFFF;
+    height: 1px;
+  }
+
+  &__default {
+    gap: 8px;
+    border-radius: 8px;
+    padding: 8px 8px;
+
+    &--no-hover {
+      cursor: default;
+    }
+  }
+
+  .item__default:not(.item__default--no-hover):hover {
+    background-color: #FFFFFF;
+    cursor: pointer;
+  }
+
+  &__body {
+    display: flex;
+    align-items: center;
+    min-height: 20px;
+    gap: 8px;
+  }
+}
+</style>
